6024ce170162ce397fb0c723a0efda08f9ee6ab6
leon-romero-origolab <leonardo@origoconsultores.cl>
2020-08-04 13:07:10 -0400

    add users table

--- database/migrations/2020_08_04_124827_create_users_table.php
+++ database/migrations/2020_08_04_124827_create_users_table.php
@@ -15,11 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('first_name',50);
+            $table->string('last_name',50);
+            $table->string('email',150)->unique();
+            $table->string('password',64);
+            $table->enum('admin',[0,1])->default('1');
+            $table->string('run',12)->unique();
+            $table->string('photo')->default("/img/img-default-user.jpg");
+            $table->string('lang',3)->default('es');
             $table->timestamps();
         });
     }
