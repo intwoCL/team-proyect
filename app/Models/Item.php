7b0e1f89b46b5808d355a10c87732d84f989b24b
leon-romero-origolab <leonardo@origoconsultores.cl>
2020-08-04 19:33:41 -0400

    Models part1
    creacion de Modelos, Testeo Pendiente

---
+++ app/Models/Item.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    //
+
+    public function content(){
+        return $this->belongsTo(Content::class,'content_id');
+    }
+    
+    public function types(){
+        return $this->hasMany(DetailItem::class,'item_id');
+	}
+}
