0b59ee458d9705f6232f8d14d16013b9bee1cb00
leon-romero-origolab <leonardo@origoconsultores.cl>
2020-08-11 20:34:44 -0400

    activity controller first activiy change

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -7,80 +7,99 @@ use App\Models\Activity;
 
 class ActivityController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $activities = Activity::get();
-        return view('admin.activity.index',compact('$activities'));
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $activities = Activity::get();
+    return view('admin.activity.index',compact('$activities'));
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    return view('admin.activity.create');
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    try {
+      $a = new Activity();
+      $a->name = $request->input('name');
+      $a->objetive = $request->input('objetive');
+      $a->scale_id = $request->input('scale_id');
+      $a->total_time = $request->input('total_time');
+      
+      if(!empty($request->file('photo'))){
+        $request->validate([
+          'photo' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+        $photo_name = time().'.'.$request->image->extension();  
+        $request->image->move(public_path('/dir/formulario/'), $photo_name);
+        $a->foto = "/dir/formulario/$photo_name";
+      } 
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+      $a->save();
+    } catch (\Throwable $th) {
+      return $th;
     }
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
 }
---
+++ resources/views/admin/activity/create.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<section class="section">
+  <div class="section-header">
+    <h1>{{trans('t.activity.create.title')}}</h1>
+    <div class="section-header-breadcrumb">
+      <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+      <div class="breadcrumb-item"><a href="#">Forms</a></div>
+      <div class="breadcrumb-item">Form Validation</div>
+    </div>
+  </div>
+
+  <div class="section-body">
+    <h2 class="section-title">Form Validation</h2>
+    <p class="section-lead">
+      Form validation using default from Bootstrap 4
+    </p>
+
+    <div class="row">
+      
+      <div class="col-12 col-md-6 col-lg-6">
+        <div class="card">
+          
+          <form action="{{route('activity.store')}}" method="POST" enctype="multipart/form-data" >
+            @csrf
+            <div class="card-header">
+              <h4>Default Validation</h4>
+            </div>
+            <div class="card-body">
+              <div class="form-group">
+                <label>{{trans('t.activity.create.name')}}</label>
+                <input type="text"  name="name" class="form-control" required="">
+              </div>
+
+              <div class="form-group">
+                <label>Objetivo</label>
+                <input type="text"  name="objetive" class="form-control" required="">
+              </div>
+
+              <div class="form-group">
+                <label>Foto</label>
+                <input type="file"  name="photo" class="form-control" required="">
+              </div>
+
+              <div class="form-group">
+                <label>Escala</label>
+                <input type="text"  name="scale_id" class="form-control" required="">
+              </div>
+
+              <div class="form-group">
+                <label>Tiempo Total</label>
+                <input type="number"  name="total_time" class="form-control" required="">
+              </div>
+
+            </div>
+            <div class="card-footer text-right">
+              <button class="btn btn-primary">Enviar</button>
+            </div>
+          </form>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</section>
+@endsection
