b72305863cded38e855e8a52cccb4679c517e5be
benja <benja.mora.torres@gmail.com>
2020-08-15 19:24:23 -0400

    fix partials

--- resources/views/admin/assignment/edit.blade.php
+++ resources/views/admin/assignment/edit.blade.php
@@ -11,7 +11,7 @@
     </div>
     <div class="section-body">
       <h2 class="section-title">Usuario : {{ $user->getFullName() }}</h2>
-      @include('partials._alertt')
+      @include('partials._alert')
       <form action="{{ route('assignment.store') }}" method="post">
         @csrf
         <input type="hidden" name="id" value="{{ $user->id }}" hidden>
