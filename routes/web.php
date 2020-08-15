84292a4f81eb4bcb30be64537f6a0245871f4801
benja <benja.mora.torres@gmail.com>
2020-08-14 22:10:15 -0400

    routes update

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,14 @@ Route::post('/', 'Auth\AuthUserController@login')->name('login');
 
 Route::middleware('user')->group(function () {
   Route::get('dashboard', 'DashboardController@index')->name('dashboard.index');
+  
   Route::resource('activity', 'ActivityController');
   Route::get('activity/{id}/content/create', 'ContentController@create')->name('content.create');
   Route::resource('user', 'UserController');
   Route::resource('calendar', 'CalendarController');
   Route::resource('assignment', 'AssignmentController');
   Route::resource('attention', 'AttentionController');
+  
   
 });
 
